9b3c6e5d8db0b02fc599e06a1a5d47d2325f64e4
Stripeadults <winnerever327@gmail.com>
2026-09-14 10:17:18 +0300

    Add package constants and support information

--- src/constants/packages.ts
+++ src/constants/packages.ts
@@ -1,1 +1,34 @@
-j
+export const PACKAGES = [
+  { id: 'galaxy-a03', name: 'Galaxy A03 Starter', price: 15000, dailyIncome: 1500, duration: 30, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a03-2.jpg' },
+  { id: 'galaxy-a05', name: 'Galaxy A05', price: 30000, dailyIncome: 3000, duration: 45, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a05-1.jpg' },
+  { id: 'galaxy-a15', name: 'Galaxy A15', price: 70000, dailyIncome: 7000, duration: 60, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a15-1.jpg' },
+  { id: 'galaxy-a25', name: 'Galaxy A25', price: 125000, dailyIncome: 12500, duration: 60, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a25-5g-1.jpg' },
+  { id: 'galaxy-a35', name: 'Galaxy A35', price: 260000, dailyIncome: 26000, duration: 60, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a35-1.jpg' },
+  { id: 'galaxy-a55', name: 'Galaxy A55', price: 400000, dailyIncome: 40000, duration: 60, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a55-1.jpg' },
+  { id: 'galaxy-s23-fe', name: 'Galaxy S23 FE', price: 900000, dailyIncome: 90000, duration: 90, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s23-fe-1.jpg' },
+  { id: 'galaxy-s24', name: 'Galaxy S24', price: 1600000, dailyIncome: 160000, duration: 90, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-1.jpg' },
+  { id: 'galaxy-s24-plus', name: 'Galaxy S24+', price: 2802000, dailyIncome: 280200, duration: 120, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-plus-1.jpg' },
+  { id: 'galaxy-s24-ultra', name: 'Galaxy S24 Ultra', price: 4500000, dailyIncome: 450000, duration: 120, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-ultra-1.jpg' },
+  { id: 'galaxy-z-flip6', name: 'Galaxy Z Flip6', price: 5000000, dailyIncome: 500000, duration: 120, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-z-flip6-1.jpg' },
+  { id: 'galaxy-z-fold6', name: 'Galaxy Z Fold6 VIP', price: 7000000, dailyIncome: 700000, duration: 180, image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-z-fold6-1.jpg' },
+];
+export type Package = typeof PACKAGES[number];
+export const REGISTRATION_BONUS = 7000;
+export const REFERRAL_BONUS = 2000;
+export const MIN_WITHDRAWAL = 7000;
+export const MIN_DEPOSIT = 15000;
+export const DAILY_CHECKIN_REWARD = 500;
+export const CHECKIN_REWARD = 500;
+export const DAILY_CHECKIN = 500;
+export const TELEGRAM_OFFICIAL = "https://t.me/samsung_ug_official";
+export const TELEGRAM_CHANNEL = "https://t.me/samsung_ug_official";
+export const TELEGRAM_SUPPORT = "https://t.me/samsung_ug_official";
+export const TELEGRAM_LINK = "https://t.me/samsung_ug_official";
+export const WHATSAPP_SUPPORT = "https://wa.me/256700000000";
+export const WHATSAPP_OFFICIAL = "https://wa.me/256700000000";
+export const SUPPORT_EMAIL = "support@samsungearnings.ug";
+export const OFFICIAL_WEBSITE = "https://samsungearnings.ug";
+export const MTN_NUMBER = "0780000000";
+export const MTN_NAME = "Samsung Earnings UG";
+export const AIRTEL_NUMBER = "0700000000";
+export const AIRTEL_NAME = "Samsung Earnings UG";
