35ba1a9b430cd843debc08b700530e8937331914
Stripeadults <winnerever327@gmail.com>
2026-09-15 08:58:42 +0300

    Add package constants and configuration

--- src/constants/packages.ts
+++ src/constants/packages.ts
@@ -1,1 +1,132 @@
-d
+export const PACKAGES = [
+  {
+    id: 'galaxy-a05',
+    name: 'Galaxy A05',
+    price: 15000,
+    dailyIncome: 3000,
+    duration: 60,
+    totalReturn: 180000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a05-1.jpg'
+  },
+  {
+    id: 'galaxy-a15',
+    name: 'Galaxy A15',
+    price: 30000,
+    dailyIncome: 7000,
+    duration: 60,
+    totalReturn: 420000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a15-1.jpg'
+  },
+  {
+    id: 'galaxy-a25',
+    name: 'Galaxy A25',
+    price: 50000,
+    dailyIncome: 12500,
+    duration: 180,
+    totalReturn: 2250000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a25-1.jpg'
+  },
+  {
+    id: 'galaxy-a35',
+    name: 'Galaxy A35',
+    price: 100000,
+    dailyIncome: 26000,
+    duration: 180,
+    totalReturn: 4680000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a35-1.jpg'
+  },
+  {
+    id: 'galaxy-a55',
+    name: 'Galaxy A55',
+    price: 150000,
+    dailyIncome: 40000,
+    duration: 180,
+    totalReturn: 7200000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a55-1.jpg'
+  },
+  {
+    id: 'galaxy-s23-fe',
+    name: 'Galaxy S23 FE',
+    price: 300000,
+    dailyIncome: 90000,
+    duration: 210,
+    totalReturn: 18900000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s23-fe-1.jpg'
+  },
+  {
+    id: 'galaxy-s24',
+    name: 'Galaxy S24',
+    price: 500000,
+    dailyIncome: 160000,
+    duration: 210,
+    totalReturn: 33600000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-1.jpg'
+  },
+  {
+    id: 'galaxy-s24-plus',
+    name: 'Galaxy S24+',
+    price: 800000,
+    dailyIncome: 280200,
+    duration: 210,
+    totalReturn: 58842000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-plus-5g-1.jpg'
+  },
+  {
+    id: 'galaxy-s24-ultra',
+    name: 'Galaxy S24 Ultra',
+    price: 1200000,
+    dailyIncome: 450000,
+    duration: 210,
+    totalReturn: 94500000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s24-ultra-5g-1.jpg'
+  },
+  {
+    id: 'galaxy-z-flip6',
+    name: 'Galaxy Z Flip6',
+    price: 1300000,
+    dailyIncome: 500000,
+    duration: 210,
+    totalReturn: 105000000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-z-flip6-1.jpg'
+  },
+  {
+    id: 'galaxy-z-fold6',
+    name: 'Galaxy Z Fold6 VIP',
+    price: 2000000,
+    dailyIncome: 805000,
+    duration: 210,
+    totalReturn: 169050000,
+    image: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-z-fold6-1.jpg'
+  },
+];
+
+export type Package = typeof PACKAGES[number];
+
+export const REGISTRATION_BONUS = 7000;
+export const REFERRAL_BONUS = 2000;
+export const MIN_WITHDRAWAL = 7000;
+export const MIN_DEPOSIT = 15000;
+
+// FIXED: No extra daily reward - user gets ONLY package dailyIncome
+export const DAILY_CHECKIN_REWARD = 0;
+export const CHECKIN_REWARD = 0;
+export const DAILY_CHECKIN = 0;
+
+export const WITHDRAWAL_FEE_PERCENT = 18;
+export const WITHDRAWAL_FEE = 18;
+export const WITHDRAWAL_TAX = 18;
+export const WITHDRAWAL_CHARGE = 18;
+
+export const TELEGRAM_OFFICIAL = "https://t.me/samsung_ug_official";
+export const TELEGRAM_CHANNEL = "https://t.me/samsung_ug_official";
+export const TELEGRAM_SUPPORT = "https://t.me/samsung_ug_official";
+export const TELEGRAM_LINK = "https://t.me/samsung_ug_official";
+export const WHATSAPP_SUPPORT = "https://wa.me/256700000000";
+export const WHATSAPP_OFFICIAL = "https://wa.me/256700000000";
+export const SUPPORT_EMAIL = "support@samsungearnings.ug";
+export const OFFICIAL_WEBSITE = "https://samsungearnings.ug";
+
+export const MTN_NUMBER = "0780000000";
+export const MTN_NAME = "Samsung Earnings UG";
+export const AIRTEL_NUMBER = "0700000000";
+export const AIRTEL_NAME = "Samsung Earnings UG";
